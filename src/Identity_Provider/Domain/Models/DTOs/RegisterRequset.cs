9677159dcf7388affcb3ca0606bf75e39b6fb1f1
Kseniia-oss <greenksenia0@gmail.com>
2026-04-24 23:52:11 +0300

    Create RegisterRequset.cs

---
+++ src/Identity_Provider/Domain/Models/DTOs/RegisterRequset.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.DTOs
+{
+    public class RegisterRequset
+    {
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
