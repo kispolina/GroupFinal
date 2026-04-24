01cbf57e9520b4a3410dcb69e1729db0d8a3f6a8
Kseniia-oss <greenksenia0@gmail.com>
2026-04-24 23:48:45 +0300

    Create AuthResponce.cs

---
+++ src/Identity_Provider/Domain/Models/DTOs/AuthResponce.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.DTOs
+{
+    public class AuthResponce
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
